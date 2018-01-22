08ebedbf0bf31cd10caddf659b91e1886f9a7d2f
pauliuskalv <pauliuskalv@gmail.com>
2018-01-22 18:41:19 +0200

    Import OpenCV module to project

--- app/src/main/java/com/unixonly/fooslive/MenuActivity.java
+++ app/src/main/java/com/unixonly/fooslive/MenuActivity.java
@@ -2,6 +2,13 @@ package com.unixonly.fooslive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.osgi.OpenCVInterface;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +16,14 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        if (!OpenCVLoader.initDebug())
+        {
+            // Initialization error
+            System.out.println("Error while loading native libraries!");
+            System.exit(0);
+        }
+        // Check if OpenCV works by using a class from the API
+        Mat mat = new Mat();
+        mat = null;
     }
 }
