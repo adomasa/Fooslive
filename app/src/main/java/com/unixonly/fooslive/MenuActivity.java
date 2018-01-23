36c6cbb94192eabff7a70bce206ef5e29f7db91d
pauliuskalv <pauliuskalv@gmail.com>
2018-01-24 00:41:24 +0200

    Import OpenCV 3.3.1 to project

--- app/src/main/java/com/unixonly/fooslive/MenuActivity.java
+++ app/src/main/java/com/unixonly/fooslive/MenuActivity.java
@@ -2,6 +2,12 @@ package com.unixonly.fooslive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.FeatureDetector;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +15,12 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        // Load binary files and enable hardware acceleration
+        if (!OpenCVLoader.initDebug(true)) {
+            // Error while loading binary files
+            Log.w("Initialization error:", "Error while initializing!");
+            System.exit(0);
+        }
     }
 }
