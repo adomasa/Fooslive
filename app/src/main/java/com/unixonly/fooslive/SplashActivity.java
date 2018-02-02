5e9b7cf4cc482f7119c3229a930f3bbfa918e326
Adomas <31891228+ramumnesys@users.noreply.github.com>
2018-02-02 23:14:51 +0200

    Update application's resource files Apply proper resources naming conventions Delete redundant resources entries Edit resources values Add translatable tags for future translation Add initial preferences layout file Add arrays resource file
    Signed-off-by: Adomas <31891228+ramumnesys@users.noreply.github.com>

---
+++ app/src/main/java/com/unixonly/fooslive/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.unixonly.fooslive;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+        finish();
+    }
+}
