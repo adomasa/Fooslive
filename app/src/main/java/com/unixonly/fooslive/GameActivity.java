9b7b3e8946e7abea62ce75c6770b64998edd7a57
Adomas <31891228+ramumnesys@users.noreply.github.com>
2018-03-24 16:33:02 +0200

    Partly integrate PositionManager into GameActivity
    Signed-off-by: Adomas <31891228+ramumnesys@users.noreply.github.com>

--- app/src/main/java/com/unixonly/fooslive/GameActivity.java
+++ app/src/main/java/com/unixonly/fooslive/GameActivity.java
@@ -4,11 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.unixonly.fooslive.databinding.ActivityGameBinding;
+import com.unixonly.fooslive.utils.PositionManager;
 
 // TODO port activity
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements PositionManager.OnPositionManagerInteractionListener {
     private static final String TAG = "GameActivity";
 
     private ActivityGameBinding mBinding;
@@ -24,14 +26,27 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-//        TODO: if (GameMode == ECaptureMode.Live) _positionManager.StopListening();
+//        if (GameMode == ECaptureMode.Live)
+//            _positionManager.StopListening();
         finish();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-//       TODO: if (GameMode == ECaptureMode.Live) _positionManager.StartListening();
+//        if (GameMode == ECaptureMode.Live)
+//            _positionManager.StartListening();
+    }
+
+    /**
+     * TODO: move to separate UI utils class
+     * Update image visibility
+     * @param image ImageView instance reference
+     * @param toVisible visible if true, invisible if false
+     */
+    private void updateVisibility(ImageView image, boolean toVisible) {
+        if (toVisible) image.setVisibility(View.VISIBLE);
+        else image.setVisibility(View.INVISIBLE);
     }
 
     private void hideSystemUI() {
@@ -40,5 +55,21 @@ public class GameActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    /**
+     * Update guidelines images visibility based on passed flags
+     * @param flagSet visibility flags container
+     */
+    @Override
+    public void onPositionManagerCallback(int flagSet) {
+        updateVisibility(mBinding.imageArrowTop,
+                (flagSet | PositionManager.EXCEEDS_TOP) == flagSet);
+        updateVisibility(mBinding.imageArrowBot,
+                (flagSet | PositionManager.EXCEEDS_BOT) == flagSet);
+        updateVisibility(mBinding.imageArrowLeft,
+                (flagSet | PositionManager.EXCEEDS_LEFT) == flagSet);
+        updateVisibility(mBinding.imageArrowRight,
+                (flagSet | PositionManager.EXCEEDS_RIGHT) == flagSet);
     }
 }
